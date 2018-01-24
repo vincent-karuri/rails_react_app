54b850068a301fc1ea5a65d74202acf3b8eb07fd
vkaruri <vincentmkaruri@gmail.com>
2018-01-24 05:22:31 -0500

    Added idea board react front end which calls rails backend API

---
+++ idea_board/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css'
+import IdeasContainer from './components/IdeasContainer'
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Idea Board</h1>
+        </header>
+        <IdeasContainer />
+      </div>
+    );
+  }
+}
+
+export default App;
